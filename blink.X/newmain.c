ce6a83fb8945bcdcf6d7e8f21d326988b563a1de
wldn <winistein@gmail.com>
2017-05-05 20:47:27 +0700

    check if/else, works!

--- blink.X/newmain.c
+++ blink.X/newmain.c
@@ -5,9 +5,9 @@
  * Created on April 27, 2017, 7:01 PM
  */
 
-#define FCY 16000000
-#define _XTAL_FREQ 16000000
-#define __delay_ms(d) { __delay32( (unsigned long) (((unsigned long long) d)*(FCY)/1000ULL)); }
+#define FCY 16000000UL
+//#define _XTAL_FREQ 16000000
+//#define __delay_ms(d) { __delay32( (unsigned long) (((unsigned long long) d)*(FCY)/1000ULL)); }
 
 #include <xc.h>
 #include <libpic30.h>
@@ -15,22 +15,23 @@
 #include <stdlib.h>
 #include "config.h"
 
-void delay(void) {
-    long i = 65535;
-    while(i--);        
-}
 
 int main(){
-    ADPCFG = 1; // Setup A/D PORT CONFIGURATION REGISTER as digital
-    TRISB = 0; // configures port B as output
-    TRISBbits.TRISB5= 0; // configures pin RB5 of port B as output
-    LATBbits.LATB5 = 1; // set pin as high
+    // DEFINE ANALOG INPUT
     
+    
+    // DEFINE DIGITAL I/O
+    ADPCFG = 0xffff; // all pins of port B are digital
+    ADPCFGbits.PCFG5 = 1; // configures pin RB5 as digital
+    TRISBbits.TRISB5= 0; // configures pin RB5 of port B as output
+        
+    int i = 21;
     while(1){
-        LATBbits.LATB5 = 1; // set pin as high
-        delay();
-        LATBbits.LATB5 = 0; // set pin as low
-        delay();
+        if(i<20){
+            LATBbits.LATB5 = 0; // led OFF
+        } else {
+            LATBbits.LATB5 = 1; // led ON
+        }
     }
     return 0;
 }
